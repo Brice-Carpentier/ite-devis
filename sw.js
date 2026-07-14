a15e43880ba6782e78de0d85855fc6dee07db67b
Brice Carpentier <carpentierbpv@gmail.com>
2026-07-14 02:00:09 +0200

    Bump cache du service worker (v14) pour correction detection fermeture croquis

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Incrémenter ce numéro à chaque mise à jour de l'appli pour forcer le rechargement du cache.
-const CACHE_NAME = "ite-devis-cache-v13";
+const CACHE_NAME = "ite-devis-cache-v14";
 
 const APP_SHELL = [
   "./",
